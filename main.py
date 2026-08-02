6c3322322e1e10bf7fe62a180e2749e52da91db1
Ebrahim <eseravat@hotmail.com>
2026-08-02 17:00:46 +0100

    Added subroutines to request data from AV API and connected to localhost with fastapi

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from config import API_KEY
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def greet():
+    return "Portfolio Tracker"
+
+
+def get_stock(ticker: str):
+    response = requests.get(f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&interval=15min&apikey={API_KEY}")
+    data = response.json()
+    return data["Global Quote"]
+
+def get_price(ticker: str):
+    
+    return get_stock(ticker)["05. price"]
+
