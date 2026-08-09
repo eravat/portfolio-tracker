7248abce8ad1efa8e1f6c3d3b3c7f3b7180e7f63
Ebrahim <eseravat@hotmail.com>
2026-08-09 17:13:49 +0100

    completed token validation so future routes requiring authorization will be accessibkle

--- auth.py
+++ auth.py
@@ -1,11 +1,24 @@
 import bcrypt
 from datetime import timedelta, datetime
 from fastapi import Depends, HTTPException
-from pydantic import BaseModel
 from jose import jwt, JWTError
 from config import SECRET_KEY
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+from users_models import Users
+from database import session
+
+
+security = HTTPBearer()
 
 ALGORITHM="HS256"
+
+def get_db():
+    db = session()
+    try:
+        yield db        #waiting for the other function to use it
+    finally:
+        db.close()
 
 def password_hasher(original_password: str):
     salt = bcrypt.gensalt()
@@ -16,5 +29,25 @@ def check_password(user_password_input: str, hash_password: str):
     check = bcrypt.checkpw(password=user_password_input.encode('utf-8'), hashed_password=hash_password.encode('utf-8'))   #need to fetch hashed password from database based on email entered
     return check
 
-def create_access_token():
-    ...                         #not yet completed obviously
+def create_access_token(user_id: str, expires_delta: timedelta | None = None):
+    encode = {"sub": str(user_id)}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
+
+    token = credentials.credentials
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Could not validate user")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Could not validate user")      
+
+    user = db.query(Users).filter(Users.id == int(user_id)).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="Could not validate user")
+    return user
--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import requests
 from config import API_KEY
 from fastapi import FastAPI, Depends, HTTPException
-from database import session, engine
+from database import engine
 from users_models import Users, UserBase
 from sqlalchemy.orm import Session
-import bcrypt
 from models import CreateUser, LoginUser
-from auth import password_hasher, check_password, create_access_token
+from auth import password_hasher, check_password, create_access_token, get_current_user, get_db
+from datetime import timedelta
+
+
 
 app = FastAPI()
 
 UserBase.metadata.create_all(bind=engine)      #creates users tables
 
-def get_db():
-    db = session()
-    try:
-        yield db        #waiting for the other function to use it
-    finally:
-        db.close()
+
 
 
 @app.get("/")
@@ -35,11 +32,6 @@ def get_price(ticker: str):
     return get_stock(ticker)["05. price"]
 
 
-
-
-
-
-
 @app.post("/auth/register")
 def register(user: CreateUser, db: Session = Depends(get_db)):
     user.hashed_password = password_hasher(user.hashed_password)
@@ -50,9 +42,15 @@ def register(user: CreateUser, db: Session = Depends(get_db)):
 @app.post("/auth/login")
 def login(user: LoginUser, db: Session = Depends(get_db)):
     user_profile = db.query(Users).filter(Users.email==user.email).first()
+
     if user_profile and check_password(user.password, user_profile.hashed_password) == True:
-        return True
+        token = create_access_token(user_profile.id, timedelta(minutes=30)) 
+        return {"access_token": token, "token_type": "bearer"}
+
     raise HTTPException(status_code=401, detail="incorrect email or password")   #changed to an exception so client model knows request was not correctly answered
-        
+
+@app.get("/auth/info")
+def get_user_info(current_user: Users = Depends(get_current_user)):
+    return current_user
 
 
