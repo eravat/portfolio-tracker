4c9c125c4605ee47cbb027c896903c1cf89819a2
Ebrahim <eseravat@hotmail.com>
2026-08-09 17:17:36 +0100

    forgot to commit modified config.py, only a small change though

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ load_dotenv()
 
 API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
 DATABASE_URL = os.getenv("DATABASE_URL")
