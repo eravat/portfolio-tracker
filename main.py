47e1d7428170e24f710fe7ad3ac431df214de3c6
Ebrahim <eseravat@hotmail.com>
2026-08-12 13:08:32 +0100

    Added pnl endpoint which returns realised and unrealised profit and loss to the client, split up holdings into more manageable subroutines, reused subroutines in pnl endpoint

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import Session
 from models import CreateUser, LoginUser, CreatePortfolio, CreateTransaction
 from auth import password_hasher, check_password, create_access_token, get_current_user, get_db
 from datetime import timedelta, datetime
+from decimal import Decimal
 
 app = FastAPI()
 
@@ -35,6 +36,58 @@ def check_portfolio(portfolio_id: int, current_user: Users, db: Session):
     if portfolio.user_id != current_user.id:
         raise HTTPException(status_code=406, detail="Invalid portfolio id")
     return portfolio
+
+def get_quantity_and_RPL(transaction, temp_list, total_quantity, realised_PL):
+    if transaction.type == "buy":
+        temp_list.append({"quantity": transaction.quantity, "price": transaction.price})
+        total_quantity += transaction.quantity
+    elif transaction.type == "sell":
+        total_quantity -= transaction.quantity
+        quantity = transaction.quantity
+        while quantity > 0:
+            firstElement = temp_list[0]
+            firstElement["quantity"] -= quantity
+            if firstElement["quantity"] < 0:
+                original_quantity = firstElement["quantity"] + quantity
+                realised_PL += (transaction.price - firstElement["price"])*original_quantity
+                quantity = abs(firstElement["quantity"])
+                temp_list.pop(0)
+            else:
+                realised_PL += (transaction.price-firstElement["price"])*quantity
+                quantity = 0
+    return temp_list, total_quantity, realised_PL
+
+def calculate_ticker_res(db: Session, portfolio_id: int):
+    ticker_dict = {}
+    transactions = db.query(Transactions).filter(portfolio_id==Transactions.portfolio_id).order_by(Transactions.transaction_date).all()
+
+    for transaction in transactions:
+        if transaction.ticker not in ticker_dict:
+            ticker_dict[transaction.ticker] = [transaction]
+        else:
+            ticker_dict[transaction.ticker].append(transaction)
+
+    ticker_res = {}
+    for ticker in ticker_dict:
+        total_quantity = 0
+        avg_price = 0
+        realised_PL = 0
+        temp_list = []
+        for transaction in ticker_dict[ticker]:
+            temp_list, total_quantity, realised_PL = get_quantity_and_RPL(transaction, temp_list, total_quantity, realised_PL)
+        
+        if total_quantity > 0:
+            net_value=0
+            for tempDict in temp_list:
+                net_value += (tempDict["quantity"]*tempDict["price"])
+
+            avg_price = net_value/total_quantity
+
+        else:
+            avg_price = 0
+
+        ticker_res[ticker] = {"total quantity":total_quantity, "average price":avg_price, "realised pnl":realised_PL}
+    return ticker_res
 
 @app.post("/auth/register")
 def register(user: CreateUser, db: Session = Depends(get_db)):
@@ -101,50 +154,22 @@ def get_stock_price(ticker: str):
 @app.get("/portfolio/{portfolio_id}/holdings")
 def get_holdings(portfolio_id: int, current_user: Users = Depends(get_current_user), db: Session = Depends(get_db)):
     check_portfolio(portfolio_id, current_user, db)
-    ticker_dict = {}
-    transactions = db.query(Transactions).filter(portfolio_id==Transactions.portfolio_id).order_by(Transactions.transaction_date).all()
-    for transaction in transactions:
-        if transaction.ticker not in ticker_dict:
-            ticker_dict[transaction.ticker] = [transaction]
-        else:
-            ticker_dict[transaction.ticker].append(transaction)
+    
+    ticker_res = calculate_ticker_res(db, portfolio_id)
+    holdings_ticker_res = {}
+    for ticker in ticker_res:
+        holdings_ticker_res[ticker] = {"total quantity":ticker_res[ticker]["total quantity"], "average price":ticker_res[ticker]["average price"]}
+    return holdings_ticker_res
 
-    ticker_res = {}
-    for ticker in ticker_dict:
-        total_quantity = 0
-        avg_price = 0
-        realised_PL = 0
-        temp_list = []
-        for transaction in ticker_dict[ticker]:
-            if transaction.type == "buy":
-                temp_list.append({"quantity": transaction.quantity, "price": transaction.price})
-                total_quantity += transaction.quantity
-            elif transaction.type == "sell":
-                total_quantity -= transaction.quantity
-                quantity = transaction.quantity
-                while quantity > 0:
-                    firstElement = temp_list[0]
-                    firstElement["quantity"] -= quantity
-                    if firstElement["quantity"] < 0:
-                        original_quantity = firstElement["quantity"] + quantity
-                        realised_PL += (transaction.price - firstElement["price"])*original_quantity
-                        quantity = abs(firstElement["quantity"])
-                        temp_list.pop(0)
-                    else:
-                        realised_PL += (transaction.price-firstElement["price"])*quantity
-                        quantity = 0
-        
-        if total_quantity > 0:
-            net_value=0
-            for tempDict in temp_list:
-                net_value += (tempDict["quantity"]*tempDict["price"])
+@app.get("/portfolio/{portfolio_id}/holdings/pnl")
+def get_pnl(portfolio_id: int, current_user: Users = Depends(get_current_user), db: Session = Depends(get_db)):
+    check_portfolio(portfolio_id, current_user, db)
+    ticker_res = calculate_ticker_res(db, portfolio_id)
+    pnl_ticker_res = {}
+    for ticker in ticker_res:       
+        current_price = get_price(ticker)
+        unrealised_pnl = (Decimal(current_price) - (ticker_res[ticker]["average price"])) * ticker_res[ticker]["total quantity"]
+        pnl_ticker_res[ticker] = {"realised pnl":ticker_res[ticker]["realised pnl"], "unrealised pnl":unrealised_pnl}
+    return pnl_ticker_res
 
-            avg_price = net_value/total_quantity
-
-        else:
-            avg_price = 0
-
-        ticker_res[ticker] = {"total quantity":total_quantity, "average price":avg_price, "realised P&L":realised_PL}
-
-    return ticker_res
 
