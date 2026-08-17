db6ab77d8e6ee8f8dea6ed6b9e893b66e7d76183
Ebrahim <eseravat@hotmail.com>
2026-08-17 17:49:52 +0100

    Added to docker to make deployment easier and to allow reproducibility on other devices

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY requirements.txt ./
+
+RUN pip install --no-cache-dir -r requirements.txt  
+#RUN keywork for running the image
+
+COPY . .
+
+ENV PORT=8000
+
+EXPOSE 8000
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+#CMD keywork for running the container
--- alembic/versions/3add2b699547_add_unique_constraint_to_email.py
+++ alembic/versions/3add2b699547_add_unique_constraint_to_email.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '3add2b699547'
-down_revision: Union[str, Sequence[str], None] = '18d1de88a8ce'
+down_revision: Union[str, Sequence[str], None] = '71e53f44ba1f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
